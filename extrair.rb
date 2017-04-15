456f1cd0aeafa9806cb6b12bee14535311fc973f
Elvis Luciano <elvis.gs@gmail.com>
2017-04-15 13:29:22 -0300

    Variáveis globais convertidas em locais

--- extrair.rb
+++ extrair.rb
@@ -13,30 +13,29 @@ def mostrar_uso
   exit 1
 end
 
-$cwd = File.dirname(__FILE__)
+cwd = File.dirname(__FILE__)
 
-$sgbd = ENV['SGBD_EXTRACAO'] || 'postgres' # mssql|postgres
-$config = YAML.load_file('config/database.yml')[$sgbd]
-$data_dir = File.absolute_path($cwd + '\data').gsub('/', '\\')
+sgbd = ENV['SGBD_EXTRACAO'] || 'postgres' # mssql|postgres
+config = YAML.load_file('config/database.yml')[sgbd]
 
-$nome_bd = ARGV.shift
-$layout = (ARGV.shift || 'fiscal').to_sym
-$caminho = ARGV.shift
+nome_bd = ARGV.shift
+layout = (ARGV.shift || 'fiscal').to_sym
+caminho = ARGV.shift
 
-if not $caminho.nil? and not (File.file?($caminho) or File.directory?($caminho))
+if not caminho.nil? and not (File.file?(caminho) or File.directory?(caminho))
   puts 'Caminho invalido'
   mostrar_uso
 end
 
-$versao = Utils.detect_version(Find.find($caminho).first(2).last)
-versoes_validas = Registro.versoes[$layout]
-unless versoes_validas.include? $versao
-  puts "Versão #{$versao} é inválida para o layout '#{$layout}'. As versões válidas são: #{versoes_validas.join(', ')}"
+versao = Utils.detect_version(Find.find(caminho).first(2).last)
+versoes_validas = Registro.versoes[layout]
+unless versoes_validas.include? versao
+  puts "Versão #{versao} é inválida para o layout '#{layout}'. As versões válidas são: #{versoes_validas.join(', ')}"
   exit 1
 end
 
-if $nome_bd
-  bd_sped = DbCreator.new($config, $nome_bd, $sgbd.to_sym, $layout, $versao)
+if nome_bd
+  bd_sped = DbCreator.new(config, nome_bd, sgbd.to_sym, layout, versao)
 
   if not bd_sped.exists?
     puts 'Criando banco de dados...'
@@ -46,29 +45,29 @@ if $nome_bd
     puts 'O banco de dados já existe. A carga será complementada.'
   end
 
-  $config[:database] = $nome_bd
+  config[:database] = nome_bd
 else
   puts 'Informe o nome do banco de dados'
   mostrar_uso
 end
 
-$chaves = {}
-$cont = 0
+chaves = {}
+cont = 0
 
-Sequel.connect($config) do |db|
-  $caminho ||= $cwd + '/sped'
-  num_arquivos = File.file?($caminho) ? 1 : Dir.glob($caminho + '/*.txt').count
+Sequel.connect(config) do |db|
+  caminho ||= cwd + '/sped'
+  num_arquivos = File.file?(caminho) ? 1 : Dir.glob(caminho + '/*.txt').count
 
-  Find.find($caminho).each do |f|
+  Find.find(caminho).each do |f|
     next if not File.file?(f) or File.zero?(f)
 
-    puts ("\n[%03d/%03d] %s" % [$cont + 1, num_arquivos, File.basename(f)])
+    puts ("\n[%03d/%03d] %s" % [cont + 1, num_arquivos, File.basename(f)])
 
     total_linhas = Utils.count_lines f
 
     progressbar = ProgressBar.create(:title => 'Progresso', :format => '[%B] %p%%')
 
-    cnpj = Utils.detect_cnpj f, $layout
+    cnpj = Utils.detect_cnpj f, layout
 
     sped = File.open(f, 'r:CP850:UTF-8')
 
@@ -83,10 +82,10 @@ Sequel.connect($config) do |db|
           progressbar.progress = progresso
         end
 
-        registro = Registro.new linha, $layout, $versao
+        registro = Registro.new linha, layout, versao
         tabela = "reg_#{registro.nome}".downcase.to_sym
 
-        if $layout == :contrib
+        if layout == :contrib
           if registro.pai == '0001'
             cnpj = ''
           end
@@ -98,14 +97,14 @@ Sequel.connect($config) do |db|
           end
         end
 
-        id = $chaves[registro.nome]
+        id = chaves[registro.nome]
         if id == nil
           id = Integer(db[tabela].select{coalesce(max(:id), 0).as(:id)}.first[:id])
         end
         id += 1
-        $chaves[registro.nome] = id
+        chaves[registro.nome] = id
 
-        id_pai = $chaves[registro.pai] || ($layout == :contrib ? '1' : nil)
+        id_pai = chaves[registro.pai] || (layout == :contrib ? '1' : nil)
 
         valores = {:id => id, :id_pai => id_pai, :cnpj_pai => cnpj}.merge registro.to_h
 
@@ -121,11 +120,11 @@ Sequel.connect($config) do |db|
     end
 
     sped.close
-    $cont += 1
+    cont += 1
     progressbar.finish
   end
 
   db.drop_table? :schema_info
 
-  puts "\n#{$cont} arquivo(s) processado(s)."
+  puts "\n#{cont} arquivo(s) processado(s)."
 end
